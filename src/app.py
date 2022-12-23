abd5a3c347a10db52bd7f63fb820ce5822705e41
Marcos Vinicius <79872354+GonMarcos@users.noreply.github.com>
2022-12-23 19:21:56 -0300

    tutorial 1

--- src/app.py
+++ src/app.py
@@ -1,10 +1,4 @@
-import os, time, qiskit
-import numpy as np
-from qiskit import Aer, QuantumCircuit, execute, ClassicalRegister, QuantumRegister, BasicAer
-from qiskit.visualization import plot_bloch_multivector
-from qiskit.visualization import plot_histogram
-from math import pi, sqrt
-
+import os, time
 from .config import config
 
 
@@ -30,92 +24,51 @@ def get_formated_name(file_name):
     return os.path.join(config.IMAGES_PATH, formated_name)
 
 
-def print_circuit_test():
-    cr = ClassicalRegister(1, "cr")
-    qr = QuantumRegister(1, "qr")
-    qc = QuantumCircuit(qr, cr)
 
-    file_path = get_formated_name("quantum_circuit")
-    qc.draw(output="mpl", filename=file_path)
-
-
-def print_qubit_orientation(orientation: float):
-    if orientation == 0:
-        qr = QuantumRegister(1, "qr")
-        qc = QuantumCircuit(qr)
-        file_path = get_formated_name("quantum_circuit_start")
-        qc.draw(output="mpl", filename=file_path)
-        backend = Aer.get_backend("statevector_simulator")
-        result = execute(qc, backend).result().get_statevector(qc, decimals=3)
-        file_path = get_formated_name("quantum_multivector_start")
-        plot_bloch_multivector(result).savefig(file_path)
-
-    if orientation == 1:
-        qr_x = QuantumRegister(1, "qr")
-        qc_x = QuantumCircuit(qr_x)
-        qc_x.x(qr_x[0])
-        file_path = get_formated_name("quantum_circuit_after_x_gate")
-        qc_x.draw(output="mpl", filename=file_path)
-        backend = Aer.get_backend("statevector_simulator")
-        result = execute(qc_x, backend).result().get_statevector(qc_x, decimals=3)
-        file_path = get_formated_name("quantum_multivector_after_x_gate")
-        plot_bloch_multivector(result).savefig(file_path)
-
-
-def print_measure_classical_register():
-    cr_x = ClassicalRegister(1, "cr")
-    qr_x = QuantumRegister(1, "qr")
-    qc_x = QuantumCircuit(qr_x, cr_x)
-    qc_x.x(qr_x[0])
-    qc_x.measure(qr_x, cr_x)
-    file_path = get_formated_name("quantum_circuit_measure")
-    qc_x.draw(output="mpl", filename=file_path)
-
-
-def print_measure_classical_register_x_times(x):
-    cr_x = ClassicalRegister(1, "cr")
-    qr_x = QuantumRegister(1, "qr")
-    qc_x = QuantumCircuit(qr_x, cr_x)
-    qc_x.x(qr_x[0])
-
-    backend = BasicAer.get_backend("qasm_simulator")
-    job = execute(qc_x, backend, shots=x)
-    result = job.result()
-    count = result.get_counts(qc_x)
-    file_path = get_formated_name("quantum_histogram")
-    plot_histogram(count).savefig(file_path)
-
-
-def print_quantum_circuit_2_qubits():
-    cr = ClassicalRegister(3, "cr")
-    qr = QuantumRegister(3, "qr")
-    qc = QuantumCircuit(qr, cr)
-    qc.x(0)
-    qc.barrier()
-    qc.measure([0, 1], [0, 1])
-    file_path = get_formated_name("quantum_circuit_2_qubits")
-    qc.draw(output="mpl", filename=file_path)
-
-
-def print_quantum_circuit_all_qubits():
-    qr = QuantumRegister(5, "qr")
-    qc = QuantumCircuit(qr, name="qc")
-    qc.x(0)
-    qc.measure_all()
-    file_path = get_formated_name("quantum_circuit_2_qubits")
-    qc.draw(output="mpl", filename=file_path)
 
 
 def execute_app():
+    import numpy as np
+    from qiskit import QuantumCircuit, transpile
+    from qiskit.providers.aer import QasmSimulator
+    from qiskit.visualization import plot_histogram
     clear_previous_images()
 
-    print(Aer.backends())       # Aer library is writen in C++
-    print(BasicAer.backends())  # BasicAer library is writen in python
+    # Use Aer's qasm_simulator
+    simulator = QasmSimulator()
 
-    #print_circuit_test()
-    print_qubit_orientation(0)
-    print_qubit_orientation(1)
-    print_measure_classical_register()
-    print_measure_classical_register_x_times(1000)
-    print_quantum_circuit_2_qubits()
-    print_quantum_circuit_all_qubits()
+    # Create a Quantum Circuit acting on the q register
+    circuit = QuantumCircuit(2, 2)
+
+    # Add a H gate on qubit 0
+    circuit.h(0)
+
+    # Add a CX (CNOT) gate on control qubit 0 and target qubit 1
+    circuit.cx(0, 1)
+
+    # Map the quantum measurement to the classical bits
+    circuit.measure([0,1], [0,1])
+
+    # compile the circuit down to low-level QASM instructions
+    # supported by the backend (not needed for simple circuits)
+    compiled_circuit = transpile(circuit, simulator)
+
+    # Execute the circuit on the qasm simulator
+    job = simulator.run(compiled_circuit, run=1000)
+
+    # Grab results from the job
+    result = job.result()
+
+    # Returns counts
+    counts = result.get_counts(compiled_circuit)
+    print("\nTotal count for 00 and 11 are:",counts)
+
+    # Draw the circuit
+    file_path = get_formated_name("quantum_circuit_start")
+    circuit.draw(output="mpl", filename=file_path)
+
+    # Plot a histogram
+    file_path = get_formated_name("quantum_histogram")
+    plot_histogram(counts).savefig(file_path)
+
+    
