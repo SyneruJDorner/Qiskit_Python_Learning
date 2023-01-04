6555f68e93b2a44acf8c9e53b5f6ecefa3fd454a
justindd1994 <justindd1994@gmail.com>
2023-01-04 21:24:03 +0200

    Cleaned up some code to match next lesson

--- src/app.py
+++ src/app.py
@@ -2,7 +2,6 @@ from src.formatter.formatter import formatter
 
 
 def lesson_1():
-    import numpy as np
     from qiskit import QuantumCircuit, transpile
     from qiskit.providers.aer import QasmSimulator
     from qiskit.visualization import plot_histogram
@@ -46,7 +45,6 @@ def lesson_1():
 
 
 def lesson_2():
-    from qiskit.primitives import Sampler
     from qiskit_machine_learning.neural_networks import SamplerQNN
     from qiskit.quantum_info import SparsePauliOp
     from qiskit_machine_learning.neural_networks import EstimatorQNN
@@ -126,5 +124,87 @@ def lesson_2():
 
 
 def execute_app():
-    print("Hello World!")
-    pass
+    import matplotlib.pyplot as plt
+    import numpy as np
+    from qiskit import QuantumCircuit
+    from qiskit.algorithms.optimizers import COBYLA, L_BFGS_B
+    from qiskit.circuit import Parameter
+    from qiskit.circuit.library import RealAmplitudes, ZZFeatureMap
+    from qiskit.utils import algorithm_globals
+
+    from qiskit_machine_learning.algorithms.classifiers import NeuralNetworkClassifier, VQC
+    from qiskit_machine_learning.algorithms.regressors import NeuralNetworkRegressor, VQR
+    from qiskit_machine_learning.neural_networks import SamplerQNN, EstimatorQNN
+
+    algorithm_globals.random_seed = 42
+
+    num_inputs = 2
+    num_samples = 20
+    X = 2 * algorithm_globals.random.random([num_samples, num_inputs]) - 1
+    y01 = 1 * (np.sum(X, axis=1) >= 0)  # in { 0,  1}
+    y = 2 * y01 - 1  # in {-1, +1}
+    # y_one_hot = np.zeros((num_samples, 2))
+    # for i in range(num_samples):
+    #     y_one_hot[i, y01[i]] = 1
+
+    # for x, y_target in zip(X, y):
+    #     if y_target == 1:
+    #         plt.plot(x[0], x[1], "bo")
+    #     else:
+    #         plt.plot(x[0], x[1], "go")
+    # plt.plot([-1, 1], [1, -1], "--", color="black")
+    #plt.show()
+
+    # construct QNN
+    qc = QuantumCircuit(2)
+    feature_map = ZZFeatureMap(2)
+    ansatz = RealAmplitudes(2)
+    qc.compose(feature_map, inplace=True)
+    qc.compose(ansatz, inplace=True)
+    #savefile_path = formatter.file('Classification','png')
+    #qc.draw(output="mpl").savefig(savefile_path)
+
+    estimator_qnn = EstimatorQNN(circuit=qc, input_params=feature_map.parameters, weight_params=ansatz.parameters)
+
+    # QNN maps inputs to [-1, +1]
+    estimator_qnn.forward(X[0, :], algorithm_globals.random.random(estimator_qnn.num_weights))
+
+    # callback function that draws a live plot when the .fit() method is called
+    def callback_graph(weights, obj_func_eval):
+        objective_func_vals.append(obj_func_eval)
+        # plt.title("Objective function value against iteration")
+        # plt.xlabel("Iteration")
+        # plt.ylabel("Objective function value")
+        # plt.plot(range(len(objective_func_vals)), objective_func_vals)
+        #plt.show()
+
+    # construct neural network classifier
+    estimator_classifier = NeuralNetworkClassifier(estimator_qnn, optimizer=COBYLA(maxiter=60), callback=callback_graph)
+
+    # create empty array for callback to store evaluations of the objective function
+    objective_func_vals = []
+    plt.rcParams["figure.figsize"] = (12, 6)
+
+    # fit classifier to data
+    estimator_classifier.fit(X, y)
+
+    # return to default figsize
+    plt.rcParams["figure.figsize"] = (6, 4)
+
+    # score classifier
+    estimator_classifier.score(X, y)
+
+    # evaluate data points
+    y_predict = estimator_classifier.predict(X)
+
+    # plot results
+    # red == wrongly classified
+    for x, y_target, y_p in zip(X, y, y_predict):
+        if y_target == 1:
+            plt.plot(x[0], x[1], "bo")
+        else:
+            plt.plot(x[0], x[1], "go")
+        if y_target != y_p:
+            plt.scatter(x[0], x[1], s=200, facecolors="none", edgecolors="r", linewidths=2)
+    plt.plot([-1, 1], [1, -1], "--", color="black")
+    plt.show()
